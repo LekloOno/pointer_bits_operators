2f6f445128836ac27df44b5dcb128f6f5537a5e0
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2024-06-03 11:43:42 +0200

    u32 -> usize

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,49 @@
 #![no_std]
 
 pub trait PtrBitsOps {
-    fn add_bits_offset(self, offset: u32) -> *const u8;
-    fn sub_bits_offset(self, offset: u32) -> *const u8;
-    fn bits_align(self, align: u32) -> *const u8;
+    fn add_bits_offset(self, offset: usize) -> *const u8;
+    fn sub_bits_offset(self, offset: usize) -> *const u8;
+    fn bits_align(self, align: usize) -> *const u8;
 }
 
 pub trait MutPtrBitsOps {
-    fn add_bits_offset(self, offset: u32) -> *mut u8;
-    fn sub_bits_offset(self, offset: u32) -> *mut u8;
-    fn bits_align(self, align: u32) -> *mut u8;
+    fn add_bits_offset(self, offset: usize) -> *mut u8;
+    fn sub_bits_offset(self, offset: usize) -> *mut u8;
+    fn bits_align(self, align: usize) -> *mut u8;
 }
 
 macro_rules! prim_impl {
     ($($t:tt)*) => {
         $(
             impl PtrBitsOps for *const $t {
-                fn add_bits_offset(self, offset: u32) -> *const u8 {
-                    (self as u32 + offset) as *const u8
+                fn add_bits_offset(self, offset: usize) -> *const u8 {
+                    (self as usize + offset) as *const u8
                 }
             
-                fn sub_bits_offset(self, offset: u32) -> *const u8 {
-                    (self as u32 - offset) as *const u8
+                fn sub_bits_offset(self, offset: usize) -> *const u8 {
+                    (self as usize - offset) as *const u8
                 }
 
-                fn bits_align(self, align: u32) -> *const u8 {
-                    ((self as u32 + align) & !(align -1)) as *const u8
+                fn bits_align(self, align: usize) -> *const u8 {
+                    ((self as usize + align) & !(align -1)) as *const u8
                 }
             }
 
             impl MutPtrBitsOps for *mut $t {
-                fn add_bits_offset(self, offset: u32) -> *mut u8 {
-                    (self as u32 + offset) as *mut u8
+                fn add_bits_offset(self, offset: usize) -> *mut u8 {
+                    (self as usize + offset) as *mut u8
                 }
             
-                fn sub_bits_offset(self, offset: u32) -> *mut u8 {
-                    (self as u32 - offset) as *mut u8
+                fn sub_bits_offset(self, offset: usize) -> *mut u8 {
+                    (self as usize - offset) as *mut u8
                 }
 
-                fn bits_align(self, align: u32) -> *mut u8 {
-                    ((self as u32 + align) & !(align -1)) as *mut u8
+                fn bits_align(self, align: usize) -> *mut u8 {
+                    ((self as usize + align) & !(align -1)) as *mut u8
                 }
             }
         )*
     };
 }
 
-prim_impl!(u8 u16 u32 i8 i16 i32);
+prim_impl!(usize u8 u16 u32 i8 i16 i32);
